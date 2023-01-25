77f697dca2502bbcb7f78c87f4aa7aee3ce15112
Mara Rúbia Alves de Oliveira <40505976+mararbia@users.noreply.github.com>
2023-01-25 14:21:40 -0300

    :fire:

--- protur/script/index.js
+++
@@ -1,7 +0,0 @@
-let title = document.querySelector("#title");
-let text_p = document.querySelector("#text");
-let text_title = "Protur";
-let text_content = "Você já pensou em viajar com sua família, amigos ou fazer uma viagem romântica? Se a resposta for sim, nós podemos te judar a escolher o destino perfeito. Confira nos links abaixo nossas opções e escolha seu destino!";
-
-title.innerHTML = text_title;
-text_p.innerHTML = text_content;
