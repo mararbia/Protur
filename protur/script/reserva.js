42bf440eebfb707cd0ad9723df509c145f8bb889
João Vitor <jovifsc@gmail.com>
2023-01-29 22:55:08 -0300

    Criando sistema de datas

--- protur/script/reserva.js
+++ protur/script/reserva.js
@@ -1,17 +1,54 @@
-const valor = 399;
-const taxa = 348.99;
+let inputStart = document.getElementById("start");
+let inputEnd = document.getElementById("end");
 
-const precoTotal = document.getElementById("precoTotal");
-const precoServiço = document.getElementById("precoServiço");
-const precoNoite = document.getElementById("precoNoite");
-const slider = document.getElementById("slider");
-const diaria = document.getElementById("diaria");
+function dateDifference(date1, date2) {
+  // Converting the strings to JavaScript date objects
+  var date1 = new Date(date1);
+  var date2 = new Date(date2);
+
+  // Calculating the difference in milliseconds
+  var difference = date2 - date1;
+
+  // Calculating the difference in days
+  var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  // Returning the difference in days
+  return days;
+}
+
+let valor = 399;
+let taxa = 348.99;
+
+let precoTotal = document.getElementById("precoTotal");
+let precoServiço = document.getElementById("precoServiço");
+let precoNoite = document.getElementById("precoNoite");
+let slider = document.getElementById("slider");
+let diaria = document.getElementById("diaria");
+let button = document.getElementById("button1");
+let teste1 = document.getElementById("teste1").textContent;
+
+let x = undefined;
+let y = undefined;
+
+inputStart.addEventListener("input", () => {
+  x = inputStart.value;
+  console.log(x);
+});
+
+inputEnd.addEventListener("input", () => {
+  y = inputEnd.value;
+  console.log(y);
+});
 
 const total = () => {
-  console.log(slider.value);
-
-  precoNoite.innerText = valor * slider.value;
-  precoTotal.innerText = valor * slider.value + taxa;
-  diaria.innerText = slider.value;
+  precoNoite.innerHTML = valor * dateDifference(x, y);
+  precoTotal.innerHTML = valor * dateDifference(x, y) + taxa;
+  diaria.innerHTML = dateDifference(x, y);
 };
-total();
+
+button.addEventListener("click", () => {
+  const diffInDays = dateDifference(x, y);
+  teste1.innerHTML = diffInDays;
+  console.log(dateDifference(x, y));
+  total();
+});
